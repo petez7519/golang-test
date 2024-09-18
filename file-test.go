6be3a0191744ef47a1a4a60ba9b7103d8286e551
petez7519 <127163326+petez7519@users.noreply.github.com>
2024-09-18 13:07:08 -0700

    Update file-test.go

--- file-test.go
+++ file-test.go
@@ -10,10 +10,10 @@ func TestAdd(t *testing.T) {
     }
 }
 
-func TestMultiply(t *testing.T) {
-    result := Multiply(2, 3)
-    expected := 6
-    if result != expected {
-        t.Errorf("Multiply(2, 3) = %d; want %d", result, expected)
-    }
-}
+#func TestMultiply(t *testing.T) {
+ #   result := Multiply(2, 3)
+  #  expected := 6
+   # if result != expected {
+    #    t.Errorf("Multiply(2, 3) = %d; want %d", result, expected)
+    #}
+#}
